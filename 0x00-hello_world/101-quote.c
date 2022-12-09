23d649019861f2947e9c05a067730556cb3fe339
esosa89 <obakpoloresosa@yahoo.com>
2022-12-08 18:51:23 -0800

    commited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+{#include <stdio.h>
 /**
  * main - Prints to standard error
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-}
+}}
