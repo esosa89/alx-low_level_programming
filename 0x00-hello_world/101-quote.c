24992029ccb3094372774806e5918c576da2c96e
esosa89 <obakpoloresosa@yahoo.com>
2022-12-08 18:33:38 -0800

    and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints to standard error
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
