94e725d5c7a5e37f20e059dea012efa17e81ec1e
esosa89 <obakpoloresosa@yahoo.com>
2022-12-15 09:29:59 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,13 @@
 
 /**
  * main - is upper.
- *
+ * @c: character
  * Return: Always 0.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
